0dfcdf171c90e93eef0b40298af315733d4cad71
tianweipeng <49048147+tianweipeng@users.noreply.github.com>
2019-08-13 14:23:28 +0800

    Update lagou.py

--- LaGou/LaGou/spiders/lagou.py
+++ LaGou/LaGou/spiders/lagou.py
@@ -12,20 +12,26 @@ class LagouSpider(scrapy.Spider):
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
+
+#请求头中的Referer必须是https://www.lagou.com/jobs/list_python?labelWords=&fromSearch=true&suginput=，所以再转跳一次，将Referer置为该url，否则显示请求太频繁
     def parse(self, response):
-        #print(response.xpath('//*[@id="lg_tbar"]/div/ul/li[5]/a/text()').extract())
-        #Cookie1 = response.headers.getlist('Set-Cookie')
-        #print('后台首次写入的响应Cookies：',Cookie1)
+        yield scrapy.Request(
+            url='https://www.lagou.com/jobs/list_python?labelWords=&fromSearch=true&suginput=',
+            callback=self.zhuantiao
+        )
+
+    def zhuantiao(self, response):
         data = {
-            'first': 'true',
+            'first': 'True',
             'pn': '1',
             'kd': 'python'
         }
         yield scrapy.FormRequest(
             url = 'https://www.lagou.com/jobs/positionAjax.json?needAddtionalResult=false',
-            cookies = response.cookie,
             formdata = data,
             callback = self.afterlogin
+
+            #headers = response.headers
         )
     def afterlogin(self, response):
         print(json.loads(response.text))
